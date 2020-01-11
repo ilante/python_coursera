46d853ccff912f5fd0a5b9bc494a63b83e47336a
Immanuela Engländer <ila@Immanuelas-Air.station>
2020-01-12 00:13:16 +0100

    coursera files first commit

---
+++ ibm.py
@@ -0,0 +1,14 @@
+# print("hello")
+# import sys
+# print(sys.version)
+# print('yes')
+# frint('error')
+# print('silence')
+# print('Hello, there')
+# total_min = 43 + 42 + 57 #total lght of ablbums in min
+# total_hours = total_min/60
+# print(total_hours)
+# print(6/2)
+name= "Michael Jackson"
+x=name.find("el")
+print(x)
---
+++ test.py
@@ -0,0 +1,21 @@
+Name = "Michael Jackson"
+print(Name[0])
+print(len(Name))
+print(Name[::2]) # adding a stride to the 
+#get every second element in the range from index 0:4
+print(Name[0:5:2]) #remember index 5 is not included because it is "untill" 
+statement = Name + ' is the best'
+print(statement)
+print(Name * 3)
+print("this \\ is nice") 
+
+a= "Thriller is the sith studio album"
+print("before upper:", a)
+b=a.upper()
+print("after upper:", b)
+
+x='this is my string'
+z=x.replace('my', 'your')
+print(z)
+l=x.find("zdf")
+print(l)
